fb8b467415544766c08da86cc3e99810f092a79c
Alice Sebego <alice.etvotrecom@gmail.com>
2024-06-13 14:04:12 +0200

    feat: create theme and colorsheme for app ui

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,8 +13,38 @@ class _HomeScreenState extends State<HomeScreen> {
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Es-tu un footix ... ou pas ?',
+            style: Theme.of(context).textTheme.titleLarge),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('This is Poppins for bodyText',
+                style: Theme.of(context).textTheme.bodyLarge),
+            Text('Es-tu un footix ... ou pas ?',
+                style: Theme.of(context).textTheme.headlineLarge),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context)
+                  .colorScheme
+                  .onPrimary, 
+                foregroundColor: Theme.of(context)
+                  .colorScheme
+                  .primary,
+              ),
+              child: const Text('Button'),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor:
+          Theme.of(context).colorScheme.background,
+    );
   }
   
 }
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0F8B91);
+  static const Color background = Color(0xFFF0EDEE);
+  static const Color text = Color(0xFF0A090C);
+  static const Color accent = Color(0xFFCEBE0C);
+  static const Color error = Color(0xFFC20C0C);
+}
